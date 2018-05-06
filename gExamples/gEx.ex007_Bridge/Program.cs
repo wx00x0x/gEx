710ae6e3f12d1baf92356d2df257f9cc52dde20d
ww00w000ww <ww00w000ww@gmail.com>
2018-05-06 23:25:31 +0300

    Bridge example

--- gExamples/gEx.ex007_Bridge/Program.cs
+++ gExamples/gEx.ex007_Bridge/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace gEx.ex007_Bridge
 {
+	/*
+	 * Мост
+	 * Данный паттерн применяется чтобы разделять абстракцию и реализацию так,
+	 * чтобы они могли изменяться независимо.
+	 * 
+	 */
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Console.Title = "Bridge (Мост)";
+
+			Console.ReadKey();
 		}
 	}
 }
